c936c0b71651cfd8b33c59e7a87ecf4e5da02cc3
taka521 <521taka.m.develop@gmail.com>
2019-12-05 01:08:10 +0900

    認証サーバの実装終わり！（多分）やったぜ

--- README.md
+++ README.md
@@ -21,3 +21,40 @@ https://pusher.com/chatkit
 ## 参考資料
 
 * https://www.sitepoint.com/pusher-vue-real-time-chat-app/
+
+
+## 環境構築
+
+### Chatkitの登録
+
+Chatkitに登録し、インスタンスを作成してください。
+
+<img src="./img/001.png" width="50%">
+
+<img src="./img/002.png" width="50%">
+
+「Credentials」を開き、『Instance Locator』『Secret Key』『Your Test Token Provdider Endpoint』の値を控えておいてください。  
+初期状態は『Your Test Token Provdider Endpoint』は存在しないので、「ENABLED?」にチェックをつけてください。  
+<img src="./img/003.png" width="50%">
+
+
+### 環境変数の設定
+
+auth-server と chat-app プロジェクト配下で以下の環境変数を設定してください。  
+`direnv`の利用を想定しています。
+
+* auth-server
+
+```
+CHATKIT_INSTANCE_LOCATOR=[your instance locator]
+CHATKIT_PRIVATE_KEY=[your instance private key]
+CHATKIT_TEST_TOKEN_PROVIDER_ENDPOINT=[your instance test token provider endpoint]
+```
+
+* chat-app
+
+```
+CHATKIT_INSTANCE_LOCATOR=[your instance locator]
+TOKEN_PROVIDER_URL=http:localhost:3001/auth
+```
+
--- auth-server/index.js
+++ auth-server/index.js
@@ -1,20 +1,163 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
+const Chatkit = require("@pusher/chatkit-server")
 
 const app = express()
 
+// =======================================
+// settings
+// =======================================
 
 // body parser settings
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// chatkit
+const chatkit = new Chatkit.default({
+  instanceLocator: process.env.CHATKIT_INSTANCE_LOCATOR,
+  key: process.env.CHATKIT_PRIVATE_KEY
+});
 
+
+// =======================================
 // routes
-app.get("/", (req, res) => {
+// =======================================
+
+app.all("/", async (req, res) => {
   res.send('うぇるかむ　おーすさーばー')
 })
 
+/*
+  == ログインAPI ==
+
+  クライアントから提出されたID・PWを検証し、問題なければ200を返却する。
+
+  今回は認証処理の実装が面倒なので、
+  指定されたIDのユーザがChatkitにユーザが存在すればOKにしている。
+*/
+app.post("/login", async (req, res) => {
+  console.log(`=== start ${req.path} ===`);
+  console.log("request header : ", req.headers)
+  console.log("request body : ", req.body);
+
+  // ごみのようなバリデーションチェック
+  const { id, pw } = req.body;
+  try {
+    validator().notEmpty("id", id).notEmpty("パスワード", pw);
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).send(e.message).end();
+    return;
+  }
+
+  // Chatkit にユーザが存在するか問い合わせ
+  chatkit.getUser({
+    id: id
+  }).then(() => {
+    console.log("login success.")
+    res.status(200).send("OK");
+  }).catch((err) => {
+    console.log(err);
+    res.status(400).send("user not found");
+  }).finally(() => {
+    console.log(`=== end ${req.path} ===`);
+  });
+});
+
+
+/*
+  == ユーザ作成API ==
+
+  クライアントから指定されたID・ユーザ名をもとに、Chatkitにユーザ作成リクエストを行います。
+
+  Chatkitのユーザはダッシュボードから作成するか、サーバサイドSDKを用いてユーザ作成を行うしかない。
+  そのため、自サービス側のユーザ作成時に、Chatkitのユーザも併せて作成するとよい。
+  ・https://pusher.com/docs/chatkit/reference/server-node#users
+*/
+app.put("/user", async (req, res) => {
+  console.log(`=== start ${req.path} ===`);
+  console.log("request header : ", req.headers)
+  console.log("request body : ", req.body);
+
+  // ごみのようなバリデーションチェック
+  const { id, name } = req.body;
+  try {
+    validator().notEmpty("id", id).notEmpty("name", name);
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).send(e.message).end();
+    return;
+  }
+
+  // Chatkit にユーザ作成リクエスト
+  chatkit.createUser({
+    id: id,
+    name: name
+  }).then((user) => {
+    console.log("created user.", user);
+    res.status(200).send("Created").end();
+  }).catch((err) => {
+    console.log(err);
+    res.status(err.status).end();
+  }).finally(() => {
+    console.log(`=== end ${req.path} ===`);
+  });
+
+});
+
+
+/*
+  == Chatkit用トークンエンドポイントAPI ==
+
+  想定としてはクライアントはリクエストヘッダ(Authorization)に自サービスで発行されたトークン（JWT等）を提出。
+  提出されたトークンに問題がなければChatkitに問い合わせを行い、Chatkit用のトークンを返却します。
+
+  レスポンスは、Chatkitが要求するレスポンス形式に合わせる。
+  ・https://pusher.com/docs/chatkit/reference/javascript#tokenprovider-arguments
+
+  {
+    "access_token": "<your token here>",
+    "expires_in": "<seconds until token expiry here>"
+  }
+
+*/
+app.post("/token", async (req, res) => {
+  console.log(`=== start ${req.path} ===`);
+  console.log("request header : ", req.headers)
+  console.log("request body : ", req.body);
+
+  // 提出された自サービスのトークンを検証（するつもり）
+  // const token = req.header("Authorization")
+
+  // Chatkitの認証
+  const authData = chatkit.authenticate({
+    userId: req.body.id
+  });
+  console.log("authData : ", authData);
+
+  const { access_token, expires_in } = authData.body;
+  res.status(authData.status)
+    .set(authData.headers)
+    .send({ access_token, expires_in })
+    .end();
+
+  console.log(`=== end ${req.path} ===`);
+});
+
+// =======================================
 // start server
+// =======================================
 app.listen(80)
 console.log("started auth server http://localhost:80 .")
+
+
+// =======================================
+// ユーティリティ
+// =======================================
+const validator = () => ({
+  notEmpty: function (name, value) {
+    if (!value || value === '') throw Error(`${name}が指定されていません`);
+    return this;
+  }
+});
